8da1ae51afb67b389659cbda4118d4503ec47744
anubizz77 <foxywolf984@gmail.com>
2024-12-10 16:47:44 -0400

    feat: finish project with easus

---
+++ main.js
@@ -0,0 +1,14 @@
+function displaySum () {
+ let firstNum = Number(document.getElementById
+ ('firstNum') .innerHTML); 
+
+ let secondNum = Number (document .getElementById
+  ('secondNum') .innerHTML) ;
+
+let total = firstNum + secondNum;
+document.getElementById ('answer').innerHTML = ` $
+{firstNum} + $ {secondNum}, equals to $ {total}` ; 
+ }
+
+ DocumentFragment.getElementById('sumButton').addEventListener
+ ("click", displaySum) ;
